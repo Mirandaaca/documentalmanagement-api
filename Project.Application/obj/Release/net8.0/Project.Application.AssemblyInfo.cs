908311115e2d04b98935bd37a601e2a5cca08ae9
Cristopher Miranda <cristopher1012z@gmail.com>
2024-11-19 19:12:58 -0400

    Presentation

--- Project.Application/obj/Release/net8.0/Project.Application.AssemblyInfo.cs
+++ Project.Application/obj/Release/net8.0/Project.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad3bbe942097238f484771b709010d82bf8d6250")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a611bd0b88f3714d8851fce8da0c48b45f0eac63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
